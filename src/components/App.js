15955f9dfad27940dc0d0fe25d5395b3fde1da83
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-20 15:44:17 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,58 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "../styles/App.css";
+import "./utils/signUpFormValidation";
+import { email, name, password } from "./utils/signUpFormValidation";
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+  const [formData, setFormdata] = useState([]);
 
+  const handleChange = (e) => {
+    console.log("e", e.target.value, e.target.name);
+    setFormdata({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <form>
+        <label for="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <div>Name is:{name}</div>
+
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <div>Email is:{email}</div>
+
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <div>Password is:{password}</div>
+
+        <label for="consent"></label>
+        <input type="checkbox" id="consent" />
+        <label for="submit"></label>
+        <button type="submit" id="submit">
+          submit
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default App;
