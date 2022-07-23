c6cc5f59bd8eafcffbccd3c8feb2a55e5aeeaa36
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-23 16:02:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,51 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
-import "./utils/signUpFormValidation";
-import { email, name, password } from "./utils/signUpFormValidation";
+import signUpFormValidation from "../utils/validation";
 
 const App = () => {
-  const [formData, setFormdata] = useState([]);
+  const formdata = {
+    name: "",
+    email: "",
+    password: ""
+  };
+  const [data, setData] = useState(formdata);
 
-  const handleChange = (e) => {
-    console.log("e", e.target.value, e.target.name);
-    setFormdata({ ...formData, [e.target.name]: e.target.value });
+  const changeName = (e) => {
+    setData({
+      name: e.target.value
+    });
+  };
+
+  const changeEmail = (e) => {
+    setData({
+      email: e.target.value
+    });
+  };
+  const changePassword = (e) => {
+    setData({
+      password: e.target.value
+    });
   };
 
   return (
     <>
-      <form>
-        <label for="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-        <div>Name is:{name}</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          signUpFormValidation(data);
+        }}
+      >
+        <label>Name</label>
+        <input type="text" id="name" onChange={changeName} value={data.name} />
 
-        <label for="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <div>Email is:{email}</div>
-
-        <label for="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <div>Password is:{password}</div>
-
-        <label for="consent"></label>
+        <label>email</label>
+        <input type="email" id="email" onChange={changeEmail} />
+        <label>password</label>
+        <input type="password" id="password" onChange={changePassword} />
+        <label>checkbox</label>
         <input type="checkbox" id="consent" />
-        <label for="submit"></label>
-        <button type="submit" id="submit">
-          submit
-        </button>
+        <label>Sign up</label>
+        <input type="submit" value="Signup" />
       </form>
     </>
   );
